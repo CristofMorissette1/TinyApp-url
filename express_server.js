b44eca8279e2f8c679715d8210531665b894ae15
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-14 17:11:43 +0000

    Updated redirections

--- express_server.js
+++ express_server.js
@@ -8,12 +8,12 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Generates random string
 function generateRandomString() {
-  Math.random().toString(36).substr(2, 6);
-
+ return Math.random().toString(36).substr(2, 6);
 }
 
-
+//
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -35,6 +35,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_news", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
@@ -45,8 +50,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);  
+  res.send("Ok");
+  let ranStr = generateRandomString();
+  urlDatabase[ranStr] = req.body.longURL;    
+  res.redirect('/urls/:shortURL');
+  console.log(urlDatabase);
 });
 
 app.listen(PORT, () => {
